4ba8396cc3ef0de20c52786256042c86cd552f5a
Joshua Carey <jdcarey128@gmail.com>
2021-03-31 16:47:06 -0600

    Modify contact tab styling and add contactCategories comp

--- src/components/ContactTab.vue
+++ src/components/ContactTab.vue
@@ -3,9 +3,12 @@
     <div class="contact-tab-title">
       Contacts
     </div>
-    <div v-for="(contact, index) in alphabet" class="contact-tab-list" :key="index">
-      <div class="contact">
+    <div v-for="(contact, index) in contactCategories" class="contact-tab-list" :key="index">
+      <div class="contact" @click="$store.dispatch('setContactsDisplay', contact)">
         {{ contact }}
+      </div>
+      <div class="contact-count">
+        xxx contacts
       </div>
     </div>
   </div>
@@ -14,9 +17,11 @@
 <script>
 export default {
   name: 'ContactTab',
-  data () {
-    return {
-      alphabet: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  computed: {
+    contactCategories () {
+      const contacts = this.$store.state.contacts.map(contact => contact.last_name[0].toUpperCase())
+      const distinctContacts = [...new Set(contacts)]
+      return distinctContacts.sort()
     }
   }
 }
@@ -26,17 +31,22 @@ export default {
 .contacts {
   color: #2c3e50;
   font-size: 20px;
+  min-height: 100vh;
   .contact-tab-title {
     padding: 20px;
     text-align: center;
     font-weight: bold;
     text-decoration: underline;
+    &:hover {
+      color: #42b983;
+      cursor: pointer;
+    }
   }
   .contact-tab-list {
     background-color: white;
     border-radius: 5px;
     padding: 10px 0px;
-    border: 1px solid #DFE3E8;
+    border: 1px solid #2c3e50;
     box-sizing: border-box;
     transition: all 0.25s ease;
     &:hover {
@@ -45,6 +55,9 @@ export default {
       transform: scale(1.1, 1.1);
       font-weight: bold;
     }
+  }
+  .contact-count {
+    font-size: 15px;
   }
 }
 </style>
