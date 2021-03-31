49078e213167b4651de3ed836fbfa6b173daecd4
Joshua Carey <jdcarey128@gmail.com>
2021-03-31 13:10:45 -0600

    Add mutation, action, getter for contacts to display

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,30 @@ import { data } from '@/assets/data.js'
 export default createStore({
   state: {
     user: data.user,
-    contacts: data.contacts
+    contacts: data.contacts,
+    contactsDisplay: ''
   },
   mutations: {
+    SET_CONTACTS_DISPLAY (state, letter) {
+      state.contactsDisplay = letter
+    }
   },
   actions: {
+    setContactsDisplay ({ commit }, letter) {
+      commit('SET_CONTACTS_DISPLAY', letter)
+    }
+  },
+  getters: {
+    getFilteredContacts (state) {
+      const contactsLetter = state.contactsDisplay
+      const filteredContacts = state.contacts.filter(contact =>
+        contact.last_name[0].toUpperCase() === contactsLetter
+      )
+      return filteredContacts
+    },
+    getAllContacts (state) {
+      return state.contacts
+    }
   },
   modules: {
   }
