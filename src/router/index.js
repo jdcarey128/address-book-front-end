7919dc5c7b09dd1e7e1fdf22e84a11adba13f0fa
Joshua Carey <jdcarey128@gmail.com>
2021-03-31 09:21:44 -0600

    Add dashboard view and route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 const routes = [
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
   {
     path: '/',
     name: 'Home',
