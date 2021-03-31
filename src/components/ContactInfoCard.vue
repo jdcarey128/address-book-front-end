683b6cb1241531607f48d6e881f1e8db69bdf892
Joshua Carey <jdcarey128@gmail.com>
2021-03-31 16:42:34 -0600

    Add styling and display for contact info card

--- src/components/ContactInfoCard.vue
+++ src/components/ContactInfoCard.vue
@@ -2,7 +2,14 @@
   <div v-if="$store.state.contactsDisplay !== ''" class="contact-info-cards">
     <h1>Contacts: {{ $store.state.contactsDisplay }}</h1>
     <div v-for="contact in $store.getters.getFilteredContacts" class="contact-info-card" :key=contact.id>
-      <p> {{ contact.first_name }}</p>
+      <p> {{ contact.first_name }} {{ contact.last_name }}</p>
+      <p v-if="contact.phone_number">Phone number: {{ contact.phone_number}}</p>
+      <p v-if="contact.street_address_2"> {{ contact.street_address }}, {{ contact.street_address_2 }}<br>
+      {{ contact.city }}, {{ contact.state }} {{ contact.zipcode }}
+      </p>
+      <p v-else> {{ contact.street_address }}<br>
+      {{ contact.city }}, {{ contact.state }} {{ contact.zipcode }}
+      </p>
     </div>
   </div>
   <div v-else>
@@ -27,5 +34,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.contact-info-card {
+  padding: 5px 20px;
+  background-color: white;
+  border-radius: 10px;
+  border: 1px solid #2c3e50;
+  cursor: pointer;
+  &:hover {
+    color: #42b983;
+    font-weight: bold;
+  }
+}
 </style>
