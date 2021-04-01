1e30e7d35ea1ed870f7ae57db7ef0d81ff500b34
Joshua Carey <jdcarey128@gmail.com>
2021-04-01 15:51:12 -0600

    Change editContact to updateContact

---
+++ src/views/UpdateContact.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='edit-contact-form-wrapper'>
+    <FormContact
+      pageTitle="Edit Contact"
+      :contact="contact"
+      method='update'
+    />
+  </div>
+</template>
+
+<script>
+import FormContact from '@/components/FormContact.vue'
+export default {
+  name: 'EditContact',
+  components: { FormContact },
+  props: {
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+  data () {
+    return {
+      contact: this.$store.getters.getContact(this.id)[0]
+    }
+  }
+}
+</script>
