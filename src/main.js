7815ad655e7c7ef5fc4926f8e63c4799aa37b7a6
Joshua Carey <jdcarey128@gmail.com>
2021-04-03 18:27:12 -0600

    Add firebase to app for auth

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA4ySzkPOdLeZD0QdPU9RBT3zdY8FO8mME',
+  authDomain: 'address-book-d188d.firebaseapp.com',
+  projectId: 'address-book-d188d',
+  storageBucket: 'address-book-d188d.appspot.com',
+  messagingSenderId: '49979778129',
+  appId: '1:49979778129:web:e0cf41abbe251ee66a4470',
+  measurementId: 'G-KFEBBG5852'
+}
+
+firebase.initializeApp(firebaseConfig)
 
 createApp(App).use(store).use(router).mount('#app')
