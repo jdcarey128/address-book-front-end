ed9a73f9437df916c3f3d5ce901736ab9d233bd1
Joshua Carey <jdcarey128@gmail.com>
2021-04-05 11:00:37 -0600

    Add redirects for unauthenticated and authenticated user

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,44 @@
     <div class='navigation_logo'>#Logo_here</div>
     <div class='navigation_user'>
       <router-link to="/">Home</router-link> |
-      <router-link to="/profile">Profile</router-link> |
-      <router-link to="#">Log Out</router-link>
+      <!-- <router-link to="/profile">Profile</router-link> | -->
+      <span @click='Logout'>Logout</span>
     </div>
   </div>
   <router-view/>
 </template>
 
 <script>
+import { onBeforeMount } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import firebase from 'firebase'
+
+export default {
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    onBeforeMount(() => {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (!user) {
+          router.replace('/login')
+        } else if (route.path === '/login' || route.path === '/register') {
+          router.replace('/')
+        }
+      })
+    })
+
+    const Logout = () => {
+      firebase
+        .auth()
+        .signOut()
+    }
+
+    return {
+      Logout
+    }
+  }
+}
 </script>
 
 <style lang="scss">
@@ -39,6 +69,14 @@
 
   .navigation_user {
     font-weight: bold;
+    span {
+      cursor: pointer;
+      font-weight: bold;
+      color: #42b983;
+      &:hover {
+        color: white;
+      }
+    }
     a {
       font-weight: bold;
       color: #42b983;
